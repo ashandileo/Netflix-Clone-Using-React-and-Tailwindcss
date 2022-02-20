a04c47a24f5cf2b91b194043639677b0de402885
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-02-20 15:13:17 +0700

    Adjust responsive banner

--- src/components/landing-page/Banner.js
+++ src/components/landing-page/Banner.js
@@ -3,13 +3,23 @@ import Background from "../../assets/img/banner.jpeg";
 const Banner = () => {
   return (
     <div className="relative mt-[-88px]">
-      <div className="text-center text-white flex flex-col items-center justify-center w-full absolute h-[86vh] top-[0px] -z-1 bg-cover" style={{ backgroundImage: `url(${Background})` }}>
-        <h1 className="text-6xl font-bold max-w-[620px]">Unlimited movies, TV shows, and more.</h1>
-        <h2 className="text-2xl font-normal mt-[16px]">Watch anywhere. Cancel anytime.</h2>
-        <h3 className="text-xl font-normal mt-[24px]">Ready to watch? Enter your email to create or restart your membership.</h3>
-        <div className="flex items-center mt-[16px] h-[60px]">
-          <input className="px-4 focus:outline-none text-black h-full w-[450px]" placeholder="Email address" />
-          <button className="bg-red-600 px-4 text-3xl h-full w-[216px]">Get Started</button>
+      <div
+        className="text-center px-[5%] text-white flex flex-col items-center justify-center w-full absolute h-[60vh] sm:h-[86vh] top-[0px] -z-1 bg-cover"
+        style={{ backgroundImage: `url(${Background})` }}
+      >
+        <h1 className="text-3xl sm:text-6xl font-bold max-w-[620px]">Unlimited movies, TV shows, and more.</h1>
+        <h2 className="text-[18px] sm:text-2xl font-normal mt-[16px]">Watch anywhere. Cancel anytime.</h2>
+        <h3 className="text-[18px] font-normal mt-[24px]">Ready to watch? Enter your email to create or restart your membership.</h3>
+        <div className="flex flex-col sm:flex-row items-center justify-center mt-[16px] w-full">
+          <input
+            className="px-4 focus:outline-none text-black h-full w-full sm:w-[450px] h-[48px] sm:h-[60px] mb-[12px] sm:mb-[0px] rounded-l-[2px] rounded-r-[2px] sm:rounded-r-[0px]"
+            placeholder="Email address"
+          />
+          <button
+            className="bg-red-600 px-4 text-base sm:text-3xl h-full w-[130px] sm:w-[216px] h-[40px] sm:h-[60px] rounded-r-[2px] rounded-r-[2px] sm:rounded-l-[0px]"
+          >
+            Get Started
+          </button>
         </div>
         <div className="-z-1 absolute bg-black bg-opacity-50 top-[0px] bottom-[0px] w-full"  />
       </div>
