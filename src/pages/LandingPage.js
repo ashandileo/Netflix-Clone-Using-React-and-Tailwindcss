540d481379ab36f1a67c08e7e1d46c1ab868a4ad
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-02-20 18:54:40 +0700

    Slicing DownloadYourShowsSection

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,14 @@
 import Header from "../components/landing-page/Header";
 import Banner from "../components/landing-page/Banner";
 import EnjoyOnYourTvSection from "../components/landing-page/EnjoyOnYourTvSection";
+import DownloadYourShowsSection from "../components/landing-page/DownloadYourShowsSection";
 
 const LandingPage = () => {
   return <>
     <Header />
     <Banner />
     <EnjoyOnYourTvSection />
+    <DownloadYourShowsSection />
   </>
 }
 
