8d4ff56d1fe0f48acb96fe2b8eb1a42443ee9256
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2022-03-06 12:03:23 +0700

    🐛 Fix can't click banner content

--- src/components/landing-page/Banner.js
+++ src/components/landing-page/Banner.js
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="relative mt-[-88px] h-[80vh] sm:h-[86vh]">
+    <div className="relative mt-[-88px] h-[80vh] sm:h-[86vh] z-10">
       <div
         className="text-center px-[5%] text-white flex flex-col items-center justify-center w-full absolute h-full top-[0px] -z-1 bg-cover"
         style={{ backgroundImage: `url(/images/banner.jpeg)` }}
@@ -13,9 +13,7 @@ const Banner = () => {
             className="px-4 focus:outline-none text-black h-full w-full sm:w-[450px] h-[48px] sm:h-[60px] mb-[12px] sm:mb-[0px] rounded-l-[2px] rounded-r-[2px] sm:rounded-r-[0px]"
             placeholder="Email address"
           />
-          <button
-            className="bg-red-600 px-4 text-base sm:text-3xl h-full w-[130px] sm:w-[216px] h-[40px] sm:h-[60px] rounded-r-[2px] rounded-r-[2px] sm:rounded-l-[0px]"
-          >
+          <button className="bg-red-600 hover:bg-red-500 px-4 text-base sm:text-3xl h-full w-[130px] sm:w-[216px] h-[40px] sm:h-[60px] rounded-r-[2px] rounded-r-[2px] sm:rounded-l-[0px]">
             Get Started
           </button>
         </div>
--- src/components/landing-page/Header.js
+++ src/components/landing-page/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="px-[5%] py-[24px] max-w-[1920px] mx-auto">
+    <header className="px-[5%] py-[24px] max-w-[1920px] mx-auto relative z-20">
       <div className="flex items-center justify-between">
         <img
           src="images/logo.svg"
@@ -8,7 +8,7 @@ const Header = () => {
           alt="Logo"
         />
         <div>
-          <button className="bg-red-600 text-white font-normal py-1 px-4 rounded text-base">
+          <button className="bg-red-600 hover:bg-red-500 text-white font-normal py-1 px-4 rounded text-base">
             Sign In
           </button>
         </div>
